4831b96281a8fe4b2013f31ec29190511d724ac8
PapaJohnsPizzeria <54676048+PapaJohnsPizzeria@users.noreply.github.com>
2019-11-03 11:09:58 -0800

    commit at home... while chillin

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,32 @@
+/*
+*
+*
+*/
+
+import java.util.*;
 
 public class PokemonBattle {
+	static Scanner userInput = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		battleStart();
+		damage();
+		
+	}
+	public static void battleStart() {
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println("Zebstrika appeared.");
+		System.out.println("Which pokemon do you choose?");
+		String pokemonName = userInput.nextLine();
+		System.out.println("You chose "+ pokemonName +"!");
+		System.out.println("It's a Pokemon battle between Zebstrika and "+ pokemonName +"! Go!");		
+	}
+	public static void damage() {
+		System.out.println("Zebstrika used Thunderbolt!");
+		System.out.println("Trainer, what are your "+ pokemonName +"'s stats?");
+		System.out.print("Level:");
+		String Level = userInput.nextLine();
+		System.out.println("Attack:");
 	}
 
 }
