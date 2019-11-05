64613c708697b927c2142d6868ace428d39a5f77
almajjoh000 <almajjoh000@CAM-LAB-16.bgsd.k12.wa.us>
2019-11-05 07:33:43 -0800

    commit.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,4 +1,17 @@
-/*
+
+/*BATTLE
+ * damage()
+ * prompt the user for their base stats in order to calculate damage
+ * damage = ((((2* level +10)/250)+(attack / defense)* base +2)* modifier)
+ * modifier = random * STAB
+ * 			  random = 0.85+(Math.random()*0.25)
+ * 
+* 
+*
+*
+*
+*
+*
 *
 *
 */
@@ -11,22 +24,36 @@ public class PokemonBattle {
 	public static void main(String[] args) {
 		battleStart();
 		damage();
-		
+
 	}
-	public static void battleStart() {
+
+	public static String battleStart() {
 		System.out.println("Another trainer is issuing a challenge!");
 		System.out.println("Zebstrika appeared.");
 		System.out.println("Which pokemon do you choose?");
-		String pokemonName = userInput.nextLine();
-		System.out.println("You chose "+ pokemonName +"!");
-		System.out.println("It's a Pokemon battle between Zebstrika and "+ pokemonName +"! Go!");		
+		String pokeName = userInput.nextLine();
+		System.out.println("You chose " + pokeName + "!");
+		System.out.println("It's a Pokemon battle between Zebstrika and " + pokeName + "! Go!");
+		return pokeName;
 	}
+
 	public static void damage() {
 		System.out.println("Zebstrika used Thunderbolt!");
-		System.out.println("Trainer, what are your "+ pokemonName +"'s stats?");
-		System.out.print("Level:");
-		String Level = userInput.nextLine();
-		System.out.println("Attack:");
+		System.out.println("Trainer, what are your \" + pokeName + \"'s stats?");
+		System.out.print("Level: ");
+		int Level = userInput.nextInt();
+		System.out.print("Attack: ");
+		int Attack = userInput.nextInt();
+		System.out.print("Defense: ");
+		int Defense = userInput.nextInt();
+		System.out.print("Base: ");
+		int Base = userInput.nextInt();
+		System.out.print("STAB: ");
+		int STAB = userInput.nextInt();
+		System.out.print("HP: ");
+		int HP = userInput.nextInt();
+
+
 	}
 
 }
